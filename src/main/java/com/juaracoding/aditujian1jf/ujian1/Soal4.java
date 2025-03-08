d5ac7355d4c5cb88c6f4728b1792b8fe416a9060
Respati <respatiaditya8@gmail.com>
2025-03-09 05:44:41 +0700

    Ujian Minggu ke – 1

--- src/main/java/com/juaracoding/aditujian1jf/ujian1/Soal4.java
+++ src/main/java/com/juaracoding/aditujian1jf/ujian1/Soal4.java
@@ -5,7 +5,21 @@ import java.util.Scanner;
 public class Soal4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] film = new String[5];
 
+        System.out.println("Daftar film bioskop");
 
+        for (int i = 0; i < film.length; i++) {
+            System.out.print("Masukkan nama film " + (i + 1) + ": ");
+            film[i] = scanner.nextLine();
+        }
+
+        System.out.println("Film yang ingin ditonton:");
+        for (int i = 0; i < film.length; i++) {
+            System.out.println((i + 1) + ". " + film[i]);
+        }
+
+        scanner.close();
     }
 }
+
